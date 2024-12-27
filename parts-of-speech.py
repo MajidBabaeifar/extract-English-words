ca07946121178fe5e9f9d4b77181be99893064ba
Majid Babaeifar <majidbabaeifar@gmail.com>
2024-12-27 23:06:17 +0100

    complete parts of speech

--- parts-of-speech.py
+++ parts-of-speech.py
@@ -1,4 +1,8 @@
 import nltk
+from collections import Counter
+import json
+
+
 
 # Download necessary resources
 nltk.download('punkt')
@@ -23,4 +27,32 @@ file_path = 'project sources/all txt/boulivard/Dienstag.txt'
 
 # Process the file and print POS tags
 pos_tags = process_text_file(file_path)
-print(pos_tags)
+
+# Count occurrences of each tuple
+counter = Counter(pos_tags)
+
+# Convert back to a list with counts
+result = [(item, count) for item, count in counter.items()]
+
+# Display the result
+print(result)
+
+
+
+# Step 1: Load second list from a JSON file
+with open('fasttext_result.json', 'r') as file:
+    english_words = json.load(file)
+
+# Your first list of tuples
+
+
+# Step 2: Apply the matching algorithm
+for item in english_words:
+    item['pos'] = []
+    for tuple_element in result:
+        if item['term'] == tuple_element[0][0]:  # Match term with the first element of the tuple
+            item['pos'].append({'role': tuple_element[0][1], 'repeat':tuple_element[1] })
+
+# Step 3: (Optional) Save updated second list back to a file
+with open('fasttext-pos-result.json', 'w') as file:
+    json.dump(english_words, file, indent=4)
